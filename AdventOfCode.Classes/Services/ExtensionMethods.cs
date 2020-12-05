936727644e66e720c383058992290ab3cc1daeae
Ross Gargett <ross.gargett@ncontracts.com>
2020-12-04 20:11:26 -0800

    Add project files.

---
+++ AdventOfCode.Classes/Services/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Classes.Services
+{
+    public static class ExtensionMethods
+    {
+        #region Parsing Methods
+
+        public static int ToInt(this object obj)
+        {
+            try
+            {
+                return Convert.ToInt32(obj);
+            }
+            catch (Exception e)
+            {
+                return Int32.MinValue;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ AdventOfCode.Classes/Services/IProblemSolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AdventOfCode.Classes
+{
+    public interface IProblemSolver
+    {
+        void ReadInputFile();
+        object SolvePartOne();
+        object SolvePartTwo();
+    }
+}
