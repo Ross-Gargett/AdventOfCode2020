8b5b3bf26b90f350378ed4b8d9fb9a64bf1546bc
Ross Gargett <ross.gargett@ncontracts.com>
2020-12-15 17:16:44 -0800

    day 13 part 2 solution

--- AdventOfCode.Day13/Day13Constants.cs
+++ AdventOfCode.Day13/Day13Constants.cs
@@ -7,7 +7,8 @@ namespace AdventOfCode.Day13
 
         public const string Day13PartOneAnswer = "Part 1:\n" +
                                                  "Multiplying the bus ID by the number of minutes you'd need to wait gives {0}\n\n";
-        public const string Day13PartTwoAnswer = "Part 2:\n";
+        public const string Day13PartTwoAnswer = "Part 2:\n" +
+                                                 "The earliest timestamp such that all of the listed bus IDs depart at offsets matching their positions in the list is {0}";
 
         #endregion
     }
--- AdventOfCode.Day13/Services/ProblemSolverDay13.cs
+++ AdventOfCode.Day13/Services/ProblemSolverDay13.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using AdventOfCode.Classes.Services;
+using AdventOfCode.Day13.Models;
 
 namespace AdventOfCode.Day13.Services
 {
@@ -11,21 +12,28 @@ namespace AdventOfCode.Day13.Services
     {
         private int earliestTime;
         public IEnumerable<int> InputLines { get; set; }
+        public IEnumerable<string> InputLinesTwo { get; set; }
 
         public void ReadInputFile()
         {
             var lines = File.ReadAllLines("Input.txt");
             earliestTime = lines[0].ToInt();
-            InputLines = lines[1].Split(',').Where(s => s != "x").Select(s => s.ToInt()).ToList();
+            InputLines = lines[1].Split(',').Where(s => s != "x").Select(s => s.ToInt()).ToList(); 
             ((List<int>)InputLines).Sort();
+                
+        }
+
+        public void ReadInputFileTwo()
+        {
+            InputLinesTwo = File.ReadAllLines("Input.txt")[1].Split(',').ToList();
         }
 
         public void SolvePartOne()
         {
             ReadInputFile();
 
-            int minRemainder = Int32.MaxValue;
-            int closestBusId = 0;
+            var minRemainder = int.MaxValue;
+            var closestBusId = 0;
 
             foreach (var busId in InputLines)
             {
@@ -42,7 +50,45 @@ namespace AdventOfCode.Day13.Services
 
         public void SolvePartTwo()
         {
-            ReadInputFile();
+            ReadInputFileTwo();
+
+            var firstTimestamp = GetFirstTimestampMatchingPattern();
+
+            Console.WriteLine(string.Format(Day13Constants.Day13PartTwoAnswer,
+                firstTimestamp));
+        }
+
+        private ulong GetFirstTimestampMatchingPattern()
+        {
+            var allBusIds = new List<BusModel>();
+            var i = 0;
+
+            foreach (var bus in InputLinesTwo)
+            {
+                if (bus != "x")
+                {
+                    allBusIds.Add(new BusModel { BusId = bus.ToInt(), Index = i });
+                }
+
+                i++;
+            }
+            
+            ulong jump = 1;
+            ulong timeStamp = 0;
+
+            for (i = 0; i < allBusIds.Count() - 1; i++)
+            {
+                var nextBus = (ulong)allBusIds[i + 1].BusId;
+                var index = (ulong)allBusIds[i + 1].Index;
+                jump *= (ulong)allBusIds[i].BusId;
+
+                while ((timeStamp + index) % nextBus != 0)
+                {
+                    timeStamp += (ulong)jump;
+                }
+            }
+
+            return timeStamp;
         }
     }
 }
