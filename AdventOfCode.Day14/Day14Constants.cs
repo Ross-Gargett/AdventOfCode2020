487cb7b840ee740033d377ddfb20d2a1d41b2ab4
Ross Gargett <ross.gargett@ncontracts.com>
2020-12-15 20:52:35 -0800

    day 14 part 1 solution

---
+++ AdventOfCode.Day14/Day14Constants.cs
@@ -0,0 +1,17 @@
+﻿
+namespace AdventOfCode.Day14
+{
+    class Day14Constants
+    {
+        #region Day 14 Constants
+
+        public const string MaskPattern = @"^mask = [X01]{36}$";
+        public const string MemNumberPattern = @"(?!mem\[)\d+(?=\])";
+
+        public const string Day14PartOneAnswer = "Part 1:\n" +
+                                                 "The sum of all values left in memory after it completes is {0}";
+        public const string Day14PartTwoAnswer = "Part 2:\n";
+
+        #endregion
+    }
+}
---
+++ AdventOfCode.Day14/Services/ProblemSolverDay14.cs
@@ -0,0 +1,74 @@
+﻿using AdventOfCode.Classes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Classes.Services;
+
+namespace AdventOfCode.Day14.Services
+{
+    class ProblemSolverDay14 : IProblemSolver<string>
+    {
+        public IEnumerable<string> InputLines { get; set; }
+
+        private ulong andMask = 0,
+                      orMask = 0;
+
+        public void ReadInputFile()
+        {
+            InputLines = File.ReadAllLines("input.txt");
+        }
+
+        public void SolvePartOne()
+        {
+            ReadInputFile();
+
+            var mem = new Dictionary<long, ulong>();
+
+            foreach (var instruction in InputLines)
+            {
+                if (Regex.IsMatch(instruction, Day14Constants.MaskPattern))
+                {
+                    ParseMasks(instruction);
+                    continue;
+                }
+
+                var memValue = instruction.Split('=');
+                var memAddress = Convert.ToInt64(Regex.Match(memValue[0], Day14Constants.MemNumberPattern).ToString());
+                var value = Convert.ToUInt64(memValue[1].Trim());
+
+                value &= andMask;
+                value |= orMask;
+                mem[memAddress] = value;
+            }
+
+            var memSum = 0ul;
+
+            foreach (var memVal in mem)
+            {
+                memSum += memVal.Value;
+            }
+
+            Console.WriteLine(string.Format(Day14Constants.Day14PartOneAnswer,
+                memSum));
+        }
+
+        public void SolvePartTwo()
+        {
+            ReadInputFile();
+        }
+
+        private void ParseMasks(string instruction)
+        {
+            var originalMask = instruction.Split('=')[1].Trim();
+
+            var andMaskStr = originalMask.Replace('X', '1');
+            var orMaskStr = originalMask.Replace('X', '0');
+
+            andMask = Convert.ToUInt64(andMaskStr, 2);
+            orMask = Convert.ToUInt64(orMaskStr, 2);
+        }
+    }
+}
